347d196a621b66d30d8d611bbc638b8e5bc07452
dawnerstart <xjreg@outlook.com>
2021-06-26 00:11:49 +0800

    建数据表

--- auth_user/onAuth.py
+++ auth_user/onAuth.py
@@ -1,29 +1,35 @@
-from django.http import HttpResponse,JsonResponse,response
-import json,simplejson,traceback
+'''
+Author: dawnerstart
+Function: 验证相关信息
+Time: 2021-06-26
+'''
+from django.http import HttpResponse, JsonResponse, response
+import json, simplejson, traceback
 from .models import User
-
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.serializers import serialize
+
+
 def register(request):
     if request.POST:
         try:
-            usernamepost=request.POST['username']
-            passwordpost=request.POST['password']
-            agepost=request.POST['age']
-            emailpost=request.POST['email']
-            phonepost=request.POST['phone']
-            addresspost=request.POST['address']
-            if(User.objects.filter(username=usernamepost) or User.objects.filter(email=emailpost)):
+            usernamepost = request.GET['username']
+            passwordpost = request.GET['password']
+            agepost = request.GET['age']
+            emailpost = request.GET['email']
+            phonepost = request.GET['phone']
+            addresspost = request.GET['address']
+            if(User.objects.filter(username = usernamepost) or User.objects.filter(email = emailpost)):
                 print('注册失败')
-                response={'result':'False','status_code':'100'}
-                return HttpResponse(json.dumps(response), content_type='application/json')
-            newuser=User(username=usernamepost,password=passwordpost,age=agepost,email=emailpost,phone=phonepost,address=addresspost)
+                response = {'result':'False', 'status_code':'100'}
+                return HttpResponse(json.dumps(response),  content_type = 'application/json')
+            newuser = User(username = usernamepost, password = passwordpost, age = agepost, email = emailpost, phone = phonepost, address = addresspost)
             newuser.save()
-            res={'result':'True','status_code':'00'}
-            return JsonResponse(data=res,safe=False)
-            print(usernamepost,res)
+            res = {'result':'True', 'status_code':'00'}
+            return JsonResponse(data = res, safe = False)
+            print(usernamepost, res)
         except:
             print('注册失败')
-            response={'result':'False','status_code':'100'}
-            return HttpResponse(json.dumps(response), content_type='application/json')
+            response = {'result':'False', 'status_code':'100'}
+            return HttpResponse(json.dumps(response),  content_type = 'application/json')
