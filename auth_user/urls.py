e11f0b2c41822817968016c9ccbce7c4d95c1498
dawnerstart <xjreg@outlook.com>
2021-06-27 18:59:14 +0800

    小改

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from . import onAuth
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/',onAuth.register),   #注册
-    path('verification_request/phone/',onAuth.get_phone_verification_code),
-    path('verification_request/email/',onAuth.get_email_verification_code),
-    path('login/phone/',onAuth.login_phone),
-    path('login/email/',onAuth.login_email),
-    path('login/email/',onAuth.change_passswd_pnone),
+    path('register/',views.register),   #注册
+    path('verification_request/phone/',views.get_phone_verification_code),
+    path('verification_request/email/',views.get_email_verification_code),
+    path('login/phone/',views.login_phone),
+    path('login/email/',views.login_email),
+    path('login/email/',views.change_passswd_pnone),
 ]
